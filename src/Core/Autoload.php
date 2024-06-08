96af6bae12ad71ce5bd65bdc6d0223ca628fbf08
richie20502 <richie.lugo.recillas.1990@gmail.com>
2024-06-08 07:42:35 -0600

    se genero el archivo autoload

--- src/Core/Autoload.php
+++ src/Core/Autoload.php
@@ -1,8 +1,23 @@
 <?php
 
-class Autoload {
+namespace App\Core;
 
+class Autoload {
     public static function register(){
-        
+        spl_autoload_register(function ($class){
+            $prefix = 'App\\';
+            $base_dir = __DIR__.'/../';
+            $len = strlen($prefix);
+            if( strncmp($prefix, $base_dir, $len ) !== 0){
+                return;
+            }
+            $relative_class = substr($class, $len);
+            $file = $base_dir. str_replace('\\', '/', $relative_class) .'.php';
+            if(file_exists($file)){
+                require $file;
+            }
+        });   
     }
 }
+
+Autoload::register();
