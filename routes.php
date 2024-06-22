9945076891014e56f02668053d9c57033b6f0ebe
richie20502 <richie.lugo.recillas.1990@gmail.com>
2024-06-22 07:13:45 -0600

    sistema de rutas 2

--- routes.php
+++ routes.php
@@ -5,6 +5,8 @@ use App\Core\Router;
 $router = new Router();
 
 $router->add('/lista/usuarios', 'HomeController@index');
+$router->add('/login', 'CONTROLADOR NUEVIO@funcionnueva');
+
 
 
 return $router;
